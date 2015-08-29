099665e6a860f435a9249d5234e8b40b45cdcd83
Peter Liu <onhout@gmail.com>
2015-08-28 19:55:20 -0700

    damn fu

--- server/config/routes.js
+++ server/config/routes.js
@@ -5,4 +5,16 @@ module.exports = function(app){
     app.get('/', function(req, res){
         res.render('index');
     });
+    app.get('/champions', function(req, res){
+        res.render('champions');
+    });
+    app.get('/firsts', function(req, res){
+        res.render('firsts');
+    });
+    app.get('/topten', function(req, res){
+        res.render('topten');
+    });
+    app.get('/teams', function(req, res){
+        res.render('teams');
+    });
 };

afc5f75e3339a7e1216dcfe333c6a8b5ef61a323
Peter Liu <onhout@gmail.com>
2015-08-30 20:14:52 -0700

    hover bar text

--- client/src/compiled/compiled.js
+++ client/src/compiled/compiled.js
@@ -78,8 +78,17 @@ var createGraph = function(barData){
             return ((HEIGHT - MARGINS.bottom) - yRange(d.values));
         })
         .attr('fill', 'darkred')
-        .on('mouseover', function(d){console.log('Y axis: ', d.values);});
+        .on("mouseover", function(d){return tooltip.style("visibility", "visible").text(d.values);})
+        .on("mousemove", function(){return tooltip.style("top",
+            (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");})
+        .on("mouseout", function(){return tooltip.style("visibility", "hidden");});
 };
+
+var tooltip = d3.select("body")
+    .append("div")
+    .style("position", "absolute")
+    .style("z-index", "10")
+    .style("visibility", "hidden");
 
 var resetGraph = function(){
     $('#visualisation').html('');
